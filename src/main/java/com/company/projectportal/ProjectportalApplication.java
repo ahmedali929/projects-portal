cbb3a3409f265d6f4e03099cd65d66a750300997
ahmedali <ahmed.ali3509@gmail.com>
2025-07-11 12:37:06 -0400

    Spring has been set up, also created a standard template file

---
+++ src/main/java/com/company/projectportal/ProjectportalApplication.java
@@ -0,0 +1,13 @@
+package com.company.projectportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectportalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/projectportal/ProjectportalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.projectportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
