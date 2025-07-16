b4ac090551b93446105c36799fa2db793be40b15
ahmedali <ahmed.ali3509@gmail.com>
2025-07-16 12:37:22 -0400

    created JobPostActivityController that helps manage dashboard for current logged in user

--- src/main/java/com/company/projectportal/Controller/JobPostActivityController.java
+++ src/main/java/com/company/projectportal/Controller/JobPostActivityController.java
@@ -2,6 +2,9 @@ package com.company.projectportal.Controller;
 
 import com.company.projectportal.services.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,7 +23,13 @@ public class JobPostActivityController {
     public String searchJobs(Model model) {
 
         Object currentUserProfile = usersService.getCurrentUserProfile();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
+        if (!(authentication instanceof AnonymousAuthenticationToken)) {
+            String currentUsername = authentication.getName();
+            model.addAttribute("username",currentUsername);
+        }
+        model.addAttribute("user", currentUserProfile);
         return "dashboard";
     }
 
