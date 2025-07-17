04e8a7d98e63cd84034baacba1da10cfc1dd735b
ahmedali <ahmed.ali3509@gmail.com>
2025-07-17 11:54:26 -0400

    upadted the addNew method on the RecruiterProfileController to allow recruiters to make a profile with a profile photo

--- src/main/java/com/company/projectportal/Controller/RecruiterProfileController.java
+++ src/main/java/com/company/projectportal/Controller/RecruiterProfileController.java
@@ -4,6 +4,7 @@ import com.company.projectportal.entity.RecruiterProfile;
 import com.company.projectportal.entity.Users;
 import com.company.projectportal.repository.UsersRepository;
 import com.company.projectportal.services.RecruiterProfileService;
+import com.company.projectportal.util.FileUploadUtil;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -11,6 +12,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
@@ -45,6 +47,7 @@ public class RecruiterProfileController {
         return "recruiter-profile";
     }
 
+    @PostMapping("/addNew")
     public String addNew(RecruiterProfile recruiterProfile, @RequestParam("image") MultipartFile multipartFile, Model model) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (!(authentication instanceof AnonymousAuthenticationToken)) {
@@ -62,6 +65,14 @@ public class RecruiterProfileController {
         RecruiterProfile savedUser = recruiterProfileService.addNew(recruiterProfile);
 
         String uploadDir = "photos/recruiter"+savedUser.getUserAccountId();
+
+        try {
+            FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return "redirect:/dashboard";
     }
 
 }
