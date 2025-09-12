cb8ecabeb71a8e28c894c7c2b1764cff2b9a9f2e
Ahmed Ali <ahmed.ali3509@gmail.com>
2025-09-12 12:30:37 -0400

    creating getRecruiterJobs method in jobpostactivity repo. need to make irecrutierjobs object

--- src/main/java/com/company/projectportal/repository/JobPostActivityRepository.java
+++ src/main/java/com/company/projectportal/repository/JobPostActivityRepository.java
@@ -2,6 +2,9 @@ package com.company.projectportal.repository;
 
 import com.company.projectportal.entity.JobPostActivity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface JobPostActivityRepository extends JpaRepository<JobPostActivity, Integer> {
+
+    List<IRecruiterJobs> getRecruiterJobs(@Param("recruiter") int recruiter);
 }
