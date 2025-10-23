5398f545e5d0a3a034a9e39108212a4db62f637f
Ahmed Ali <ahmed.ali3509@gmail.com>
2025-10-23 12:55:08 -0400

    updated JobSeekerApplyController to get list of candidates who applied for and/or saved the job

--- src/main/java/com/company/projectportal/Controller/JobSeekerApplyController.java
+++ src/main/java/com/company/projectportal/Controller/JobSeekerApplyController.java
@@ -1,7 +1,11 @@
 package com.company.projectportal.Controller;
 
 import com.company.projectportal.entity.JobPostActivity;
+import com.company.projectportal.entity.JobSeekerApply;
+import com.company.projectportal.entity.JobSeekerSave;
 import com.company.projectportal.services.JobPostActivityService;
+import com.company.projectportal.services.JobSeekerApplyService;
+import com.company.projectportal.services.JobSeekerSaveService;
 import com.company.projectportal.services.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,21 +13,29 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.List;
+
 @Controller
 public class JobSeekerApplyController {
 
     private final JobPostActivityService jobPostActivityService;
     private final UsersService usersService;
+    private final JobSeekerApplyService jobSeekerApplyService;
+    private final JobSeekerSaveService jobSeekerSaveService;
 
     @Autowired
-    public JobSeekerApplyController(JobPostActivityService jobPostActivityService, UsersService usersService) {
+    public JobSeekerApplyController(JobPostActivityService jobPostActivityService, UsersService usersService, JobSeekerApplyService jobSeekerApplyService, JobSeekerSaveService jobSeekerSaveService) {
         this.jobPostActivityService = jobPostActivityService;
         this.usersService = usersService;
+        this.jobSeekerApplyService = jobSeekerApplyService;
+        this.jobSeekerSaveService = jobSeekerSaveService;
     }
 
     @GetMapping("job-details-apply/{id}")
     public String display(@PathVariable("id") int id, Model model) {
         JobPostActivity jobDetails = jobPostActivityService.getOne(id);
+        List<JobSeekerApply> jobSeekerApplyList = jobSeekerApplyService.getJobCandidates(jobDetails);
+        List<JobSeekerSave> jobSeekerSaveList = jobSeekerSaveService.getJobCandidates(jobDetails);
         model.addAttribute("jobDetails", jobDetails);
         model.addAttribute("user", usersService.getCurrentUserProfile());
         return "job-details";
